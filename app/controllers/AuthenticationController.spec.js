eb3df66c70c495a001e802582fb87f6819f0f160
nandajl <nandajelubis@gmail.com>
2022-11-17 21:29:51 +0700

    configure database

--- app/controllers/AuthenticationController.spec.js
+++ app/controllers/AuthenticationController.spec.js
@@ -1,22 +1,37 @@
-// const AuthenticationController = require("./AuthenticationController")
+const AuthenticationController = require("./AuthenticationController")
 
-// const bcrypt = require("bcryptjs")
-// const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const { User } = require("../models")
+const { Role } = require("../models")
 
-// describe("AuthenticationController", () => {
-//     describe("#createTokenFromUser", () => {
-
+describe("AuthenticationController", () => {
+  describe("#createTokenFromUser", () => {
+    const role = {
+      id: 1,
+      name: "member"
+    }
+    const user = {
+      id: 1,
+      name: "bochi",
+      email: "bochi@mail.com",
+      image: "bochi_image",
+      role: {
+        id: role.id,
+        name: role.name
+      }
+    }
+    const authenticationController = new AuthenticationController()
+  })
+//   describe("#authorize", () => {
+//     it("should return req.user", () => {
+//       const mockRequest = {
+//         headers : {
+//             authorization :
+//         }
+//       }
+//       const mockResponse = {}
+//       const next = {}
 //     })
-    
-// //   describe("#authorize", () => {
-// //     it("should return req.user", () => {
-// //       const mockRequest = {
-// //         headers : {
-// //             authorization :
-// //         }
-// //       }
-// //       const mockResponse = {}
-// //       const next = {}
-// //     })
-// //   })
-// })
+//   })
+})
